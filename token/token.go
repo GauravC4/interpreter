4441ec6901b52186a063bb75f3450b238ae0d63e
gaurav <gaurav.mrc@gmail.com>
2025-04-02 18:36:16 +0530

    basic lexer)

---
+++ token/token.go
@@ -0,0 +1,35 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1234
+	FLOAT = "FLOAT" // 12.34
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MULTIPLY = "*"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
