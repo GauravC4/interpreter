1441c0dcd0eeddddddaefcff563fa34d0b2750d2
gaurav <gaurav.mrc@gmail.com>
2025-04-10 16:39:33 +0530

    simple repl

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/GauravC4/interpreter/repl"
+)
 
 func main() {
-	fmt.Println("hello world!")
+	fmt.Printf("Hello! This is a simple interpreter!\n")
+	fmt.Printf("Type your commands here\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
